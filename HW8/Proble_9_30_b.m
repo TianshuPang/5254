e05b96d0db230b9a95f11675591e16aaabef2843
cmuell89 <cmuell89@gmail.com>
2018-05-02 12:09:15 -0600

    HW8 problem 1 done

--- HW8/Proble_9_30_b.m
+++ HW8/Proble_9_30_b.m
@@ -2,40 +2,48 @@
 clear all;
 
 iter = 1000;
-nu = .0000001;
-beta = .5;
-alpha = .01;
+nu = .00000001;
+beta = .01;
+alpha = .50;
 n = 100;
 m = 200;
 x = zeros(n, 1);
 A = randn(m,n);
 
-NTTOL = 1e-8;
 
 x = zeros(n,1);
 
 V = []
 I = []
+T = []
 for i = 1:iter
     f = -sum(log(1-A*x)) - sum(log(1+x)) - sum(log(1-x));
     d = 1./(1-A*x);
+    % first order derivative
     grad = A'*d - 1./(1+x) + 1./(1-x);
+    % second order derivative i.e. hessian
     hessian = A'*diag(d.^2)*A + diag(1./(1+x).^2 + 1./(1-x).^2);
+    % direction
     dir = -hessian\grad;
-    fprime = grad'*dir;
-    if abs(fprime) < nu
-        break;
-    end
+    % lambda^2 i.e decrement
+    lambda_2 = grad'*dir;
     t = 1; 
     while ((max(A*(x+t*dir)) >= 1) || (max(abs(x+t*dir)) >= 1))
         t = beta*t;
     end
-    while ( -sum(log(1-A*(x+t*dir))) - sum(log(1-(x+t*dir).^2)) > f + alpha*t*fprime )
+    % breaking criteria
+    if abs(lambda_2) < nu
+        break;
+    end
+    % backtracking algorithm
+    while ( -sum(log(1-A*(x+t*dir))) - sum(log(1-(x+t*dir).^2)) > f + alpha*t*lambda_2 )
         t = beta*t;
     end
+    % update step
     x = x+t*dir;
     V = [V; f];
     I = [I ; i];
+    T = [T; t];
 end
 
 f_minus_p = [];
@@ -44,5 +52,19 @@ for i = 1:length(V)
     f_minus_p = [f_minus_p; diff]
 end
 f_minus_p
-I
+f
+figure(1)
 semilogy(I, f_minus_p)
+titlestr = "Graph of f-p* vs iterations; Alpha=%0.3f and Beta=%0.3f";
+str = sprintf(titlestr,alpha,beta);
+title(str);
+xlabel("Iterations");
+ylabel("f(x)-p*");
+
+figure(2)
+plot(I,T);
+titlestr = "Graph of t vs iterations; Alpha=%0.3f and Beta=%0.3f";
+str = sprintf(titlestr,alpha,beta);
+title(str);
+xlabel("Iterations");
+ylabel("t");
--- HW8/Problem_9_30.m
+++ HW8/Problem_9_30.m
@@ -2,8 +2,8 @@ clear all;
 
 iter = 1000;
 nu = .0001;
-beta = .5;
-alpha = .01;
+beta = .01;
+alpha = .5;
 n = 100;
 m = 200;
 x = zeros(n, 1);
@@ -11,23 +11,31 @@ A = randn(m,n);
 
 V = []
 I = []
+T = []
 for i = 1:iter
+    % function evaluation
     f = -sum(log(1-A*x))-sum(log(1+x)) - sum(log(1-x));
+    % gradient
     grad = A'*(1./(1-A*x)) - 1./(1+x) + 1./(1-x);
+    % breaking criterion using nu as threshold
     if norm(grad) < nu
         break
     end
     % Gradient direction.
     dir = -grad;
+    % second compoenent of backtracking
     fprime = grad'*dir;
     t = 1; 
     while ((max(A*(x+t*dir)) >= 1) || (max(abs(x+t*dir)) >= 1))
         t = beta*t;
     end
+    % backtracking algorithm
     while ( -sum(log(1-A*(x+t*dir))) - sum(log(1-(x+t*dir).^2)) > f + alpha*t*fprime )
         t = beta*t;
     end
+    % update step
     x = x+t*dir;
+    T = [T; t]
     V = [V; f];
     I = [I ; i]
 end
@@ -39,6 +47,21 @@ for i = 1:length(V)
 end
 f_minus_p
 f
-semilogy(I, f_minus_p)
+figure(1)
+plot(I,f_minus_p);
+set(gca,'yscale','log');
+titlestr = "Graph of f-p* vs iterations; Alpha=%0.3f and Beta=%0.3f";
+str = sprintf(titlestr,alpha,beta);
+title(str);
+xlabel("Iterations");
+ylabel("f(x)-p*");
+
+figure(2)
+plot(I,T);
+titlestr = "Graph of t vs iterations; Alpha=%0.3f and Beta=%0.3f";
+str = sprintf(titlestr,alpha,beta);
+title(str);
+xlabel("Iterations");
+ylabel("t");
 
 
